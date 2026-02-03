609b693403bb27fef3a49f72f46dce5e0ff39829
Jamelle Messinger <jamswoof914@gmail.com>
2026-02-03 10:42:27 -0700

    add try catch for additional error handling and preview of found files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import getDirectory from './cli/getDirectory.js';
 import scanDirectory from './cli/scanDirectory.js';
 
-const directory = await getDirectory();
-const files = await scanDirectory(directory);
+async function main() {
+  try {
+    const directory = await getDirectory();
+    const files = await scanDirectory(directory);
 
-console.log(`\nFound ${files.length} files.`);
+    console.log(`Found ${files.length} files`);
+    console.log('\nPreview:\n\n', files.slice(0, 3)); // preview first 3 files
+  } catch (err) {
+    console.error('Error:', err.message);
+  }
+}
+
+main();
